4acfcb7d9c1d712cf3a515ed1c82e846e3c70104
Vlad180900 <khursik.v@yandex.ru>
2023-04-25 12:19:59 +0300

    task1

--- src/Git.java
+++ src/Git.java
@@ -1,7 +1,10 @@
 public class Git {
     public static void main (String[] args) {
+        String name = "Vlad ";
+        String surname = "Khursik";
         System.out.println("Hello world");
         System.out.println(5+13);
+        System.out.println(name + surname);
 
     }
 }
